f22285b5c3bb2b33f8abe72e0a3fc1f5a5c58aa4
Gozde Arici <gozdenurarici@gmail.com>
2025-10-15 13:02:42 +0300

    added get request to /auth/register for solving 404 route not found

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -8,8 +8,17 @@ import {
   refreshUserSessionController,
   registerUserController,
 } from '../controllers/auth.js';
+import { getBasicServerController } from '../controllers/contacts.js';
 
 const authRouter = Router();
+
+authRouter.get('/register', ctrlWrapper(getBasicServerController));
+
+authRouter.get('/login', ctrlWrapper(getBasicServerController));
+
+authRouter.get('/logout', ctrlWrapper(getBasicServerController));
+
+authRouter.get('/refresh', ctrlWrapper(getBasicServerController));
 
 authRouter.post(
   '/register',
